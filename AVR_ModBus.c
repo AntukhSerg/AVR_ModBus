7f7222f5f0e55298bb9a0ccfdbf2a69a6bd9e159
AntukhSerg <77425229+AntukhSerg@users.noreply.github.com>
2022-01-11 12:24:58 +0800

    Add files via upload

---
+++ AVR_ModBus.c
@@ -0,0 +1,427 @@
+//ATmega8
+#include <avr/io.h>
+#include <avr/interrupt.h>
+#include <stdbool.h>
+#include <util/crc16.h>
+#include "AVR_ModBus.h"
+
+#define Hi(Int) (char) (Int>>8)
+#define Low(Int) (char) (Int)
+
+unsigned char RcCount, TrCount;  //счетчик принятых/переданных данных
+bool StartRec = false;// false/true начало/прием посылки
+bool bModBus = false;  //флаг обработки посылки
+unsigned char cNumRcByte0; //кол-во принятых байт
+unsigned char cNumTrByte0;  //кол-во передаваемых байт
+unsigned char cmRcBuf0[MAX_LENGHT_REC_BUF] ; //буфер принимаемых данных
+unsigned char cmTrBuf0[MAX_LENGHT_TR_BUF] ; //буфер передаваемых данных
+
+unsigned char ModBus(unsigned char NumByte);
+char Func01(void);
+char Func02(void);
+char Func03(void);
+char Func04(void);
+char Func05(void);
+char Func06(void);
+char ErrorMessage(char Error);
+unsigned int GetCRC16(unsigned char *buf, unsigned char bufsize);
+
+
+
+//настройка UART
+void InitModBus(void)
+{
+UBRRHi = Hi(BAUD_DIVIDER);
+UBRRLow = Low(BAUD_DIVIDER);
+UCSRA_CLR();
+UCSRB_SET();
+UCSRC_SET();
+//останавливаем таймер0
+StopTimer0();
+//разрешаем прерывание по переполнению таймера0
+Timer0InterruptEnable(); 
+}//end InitModBus()
+
+
+void CheckModBus(void)
+{
+if (bModBus)
+	{
+	cNumTrByte0=ModBus(cNumRcByte0); //обработка принятого соообщения ModBus
+	if (cNumTrByte0!=0)
+		{
+		TrCount=0;
+		StartTrans();
+		} //end if (cNumTrByte0!=0)
+	bModBus=false;
+	} //end if (bModBus)
+} //end CheckModBus(void)
+
+
+
+
+unsigned char ModBus(unsigned char NumByte)
+{
+int CRC16;
+unsigned char i;
+
+if (cmRcBuf0[0]!=SLAVE_ID) return 0x00; //broadcast запрос, ответ не нужен
+
+CRC16 = GetCRC16(cmRcBuf0, NumByte);
+if (CRC16) return 0;  //контрольная сумма не совпадает, ответ не нужен
+
+//проверки прошли успешно начинаем формировать ответ на запрос
+//данные для ответа заносятся в массив cmTrBuf0[]
+//!!!!! адреса и данные двубайтные заносятся старшим байтом вперед (Hi, Low)
+//!!!!! CRC заноситься младшим байтом вперед (Low, Hi) 
+
+cmTrBuf0[0]=SLAVE_ID;//адрес устройства
+
+for(i=1; i<(MAX_LENGHT_TR_BUF); i++) //очищаем буфер для данных
+	{
+	cmTrBuf0[i]=0;
+	}//end for()
+
+//код команды
+switch(cmRcBuf0[1]) 
+	{
+    case 0x01: 
+		{
+		if(!QUANTITY_REG_0X) return ErrorMessage(0x01);
+		return Func01();
+		}
+	
+	case 0x02: 
+		{
+		if(!QUANTITY_REG_1X) return ErrorMessage(0x01);
+		return Func02();
+		}
+		
+	case 0x03: 
+		{
+		if(!QUANTITY_REG_4X) return ErrorMessage(0x01);
+		return Func03();
+		}
+		
+	case 0x04: 
+		{
+		if(!QUANTITY_REG_3X) return ErrorMessage(0x01);
+		return Func04();
+		}
+		
+    case 0x05: 
+		{
+		if(!QUANTITY_REG_0X) return ErrorMessage(0x01);
+		return Func05();
+		}
+		
+	case 0x06: 
+		{
+		if(!QUANTITY_REG_4X) return ErrorMessage(0x01);
+		return Func06();
+		}
+		
+    default:   return ErrorMessage(0x01); //Принятый код функции не может быть обработан
+	} //end switch
+} //end ModBus()
+
+
+
+//функция вычисляет код CRC-16
+//на входе указатель на начало буфера
+//и количество байт сообщения (без принятого кода CRC-16)
+unsigned int GetCRC16(unsigned char *buf, unsigned char bufsize)
+{
+unsigned int crc = 0xFFFF;
+unsigned char i;
+
+for (i = 0; i < bufsize; i++) 
+crc = _crc16_update(crc, buf[i]);//подсчет CRC в принятой посылке
+return crc;
+} //end GetCRC16()
+
+
+
+//формирование ответа об ошибке
+char ErrorMessage(char Error)
+{
+int CRC16;
+
+cmTrBuf0[1]=cmRcBuf0[1]+0x80;//команда с ошибкой
+cmTrBuf0[2]=Error;
+CRC16=GetCRC16(cmTrBuf0,3);//подсчет КС посылки
+cmTrBuf0[3]=Low(CRC16);
+cmTrBuf0[4]=Hi(CRC16);
+
+return 5;
+} //end ErrorMessage()
+
+
+
+//---------------------------Функции ModBus-------------------------------
+
+#define start_adress ((cmRcBuf0[2]<<8) + cmRcBuf0[3])
+#define quantity_registers ((cmRcBuf0[4]<<8) + cmRcBuf0[5])
+#define data_byte ((quantity_registers+7)/8)
+#define data_2byte (2*quantity_registers) //данные двухбайтные
+#define max_adress (start_adress + quantity_registers)
+#define reg_adress (start_adress)
+#define value (quantity_registers)
+//побитовая запись
+#define I_byte (reg_adress/8)
+#define I_bit (reg_adress%8)
+//побитовая запись с учетом смещения адреса
+#define I_byte_ofset(x) ((start_adress+x)/8)
+#define I_bit_ofset(x) ((start_adress+x)%8)
+#define NextBit(y,x) RegNum##y[I_byte_ofset(x)] & (1<<I_bit_ofset(x))	
+#define ClrBitOfByte(ByteA, x) (ByteA &= ~(1<<(x)))
+
+
+
+//реализация функции 0х01
+//чтение значений из нескольких регистров флагов (Read Coil Status)
+char Func01(void)
+{
+unsigned int CRC16;
+unsigned char i;
+
+//проверка корректного адреса в запросе
+if(!(max_adress<=QUANTITY_REG_0X)) return ErrorMessage(0x02); //Адрес данных, указанный в запросе, недоступен
+if(!quantity_registers) return 0;//проверяем что количество регистров на чтение > 0
+
+//формируем ответ
+cmTrBuf0[1] = 0x01;//команда
+cmTrBuf0[2] = data_byte;//количество байт данных в ответе
+
+for(i=0; i<quantity_registers; i++) 
+	{
+	cmTrBuf0[3+i/8] |= ((bool)(NextBit(0x,i))<<(i%8)); //данные
+	}//end for()
+CRC16=GetCRC16(cmTrBuf0,data_byte+3);//подсчет КС посылки
+cmTrBuf0[3+data_byte]=Low(CRC16);
+cmTrBuf0[4+data_byte]=Hi(CRC16);
+return (5+data_byte);
+} //end Func01(void)
+
+
+
+
+//реализация функции 0х02
+//чтение значений из нескольких регистров флагов (Read Input Status)
+char Func02(void)
+{
+unsigned int CRC16;
+unsigned char i;
+
+//проверка корректного адреса в запросе
+if(!(max_adress<=QUANTITY_REG_1X)) return ErrorMessage(0x02); //Адрес данных, указанный в запросе, недоступен
+if(!quantity_registers) return 0;//проверяем что количество регистров на чтение > 0
+
+//формируем ответ
+cmTrBuf0[1] = 0x02;//команда
+cmTrBuf0[2] = data_byte;//количество байт данных в ответе
+
+for(i=0; i<quantity_registers; i++) 
+	{
+	cmTrBuf0[3+i/8] |= ((bool)(NextBit(1x,i))<<(i%8)); //данные
+	}//end for()
+CRC16=GetCRC16(cmTrBuf0,data_byte+3);//подсчет КС посылки
+cmTrBuf0[3+data_byte]=Low(CRC16);
+cmTrBuf0[4+data_byte]=Hi(CRC16);
+return (5+data_byte);
+} //end Func02(void)
+
+
+
+
+//реализация функции 0х03
+//чтение значений из нескольких регистров хранения (Read Holding Registers)
+char Func03(void)
+{
+unsigned int CRC16;
+unsigned char i;
+
+//проверка корректного адреса в запросе
+if(!(max_adress<=QUANTITY_REG_4X)) return ErrorMessage(0x02); //Адрес данных, указанный в запросе, недоступен
+if(!quantity_registers) return 0;//проверяем что количество регистров на чтение > 0 *******************************************************************************проверить работу***********************
+
+//формируем ответ
+cmTrBuf0[1] = 0x03;//команда
+cmTrBuf0[2] = data_2byte;//количество байт данных в ответе
+for(i=0; i<quantity_registers; i++)
+	{
+	cmTrBuf0[3+2*i] = Hi(RegNum4x[start_adress+i]);//данные старший байт
+	cmTrBuf0[4+2*i] = Low(RegNum4x[start_adress+i]);//данные младший байт
+	} //end for()
+CRC16=GetCRC16(cmTrBuf0,data_2byte+3);//подсчет КС посылки
+cmTrBuf0[3+data_2byte]=Low(CRC16);
+cmTrBuf0[4+data_2byte]=Hi(CRC16);
+return (5+data_2byte);
+} //end Func03(void)
+
+
+
+
+//реализация функции 0х04
+//чтение значения аналоговых входов (Read Input Registers)
+char Func04(void)
+{
+unsigned int CRC16;
+unsigned char i;
+
+//проверка корректного адреса в запросе
+if(!(max_adress<=QUANTITY_REG_3X)) return ErrorMessage(0x02); //Адрес данных, указанный в запросе, недоступен
+if(!quantity_registers) return 0;//проверяем что количество регистров на чтение > 0 *******************************************************************************проверить работу***********************
+
+//формируем ответ
+cmTrBuf0[1] = 0x04;//команда
+cmTrBuf0[2] = data_2byte;//количество байт данных в ответе
+for(i=0; i<quantity_registers; i++)
+	{
+	cmTrBuf0[3+2*i] = Hi(RegNum3x[start_adress+i]);//данные старший байт
+	cmTrBuf0[4+2*i] = Low(RegNum3x[start_adress+i]);//данные младший байт
+	} //end for()
+CRC16=GetCRC16(cmTrBuf0,data_2byte+3);//подсчет КС посылки
+cmTrBuf0[3+data_2byte]=Low(CRC16);
+cmTrBuf0[4+data_2byte]=Hi(CRC16);
+return (5+data_2byte);
+} //end Func04(void)
+
+
+
+
+//реализация функции 0х05
+//запись значения одного флага (Force Single Coil)
+char Func05(void)
+{
+//проверка корректного адреса в запросе
+if(!(reg_adress<=QUANTITY_REG_0X)) return ErrorMessage(0x02); //Адрес данных, указанный в запросе, недоступен
+
+//Значение FF 00 hex устанавливает флаг(1).
+//Значение 00 00 hex сбрасывает флаг (0). 
+//Другие значения не меняют флаг
+//формируем ответ, возвращая полученное сообщение
+if((value==0xFF00)||(value==0x0000))
+	{
+	if(value)
+		{
+		RegNum0x[I_byte] |= (1<<I_bit);
+		}
+	else 
+		{
+		RegNum0x[I_byte] &= ~(1<<I_bit);
+		}
+	cmTrBuf0[1] = cmRcBuf0[1];
+	cmTrBuf0[2] = cmRcBuf0[2];
+	cmTrBuf0[3] = cmRcBuf0[3];
+	cmTrBuf0[4] = cmRcBuf0[4];
+	cmTrBuf0[5] = cmRcBuf0[5];
+	cmTrBuf0[6] = cmRcBuf0[6];
+	cmTrBuf0[7] = cmRcBuf0[7];
+	return 8;
+	} //end if()
+return ErrorMessage(0x03); //Значение, содержащееся в поле данных запроса, является недопустимой величиной
+} //end Func05(void)
+
+
+
+
+//реализация функции 0х06
+//запись значения в один регистр хранения (Preset Single Register)
+char Func06(void)
+{
+//проверка корректного адреса в запросе
+if(!(reg_adress<=QUANTITY_REG_4X)) return ErrorMessage(0x02); //Адрес данных, указанный в запросе, недоступен
+//проверка корректных данных в запросе
+
+//формируем ответ, возвращая полученное сообщение
+if(1)//при необходимости поставить контроль значений
+	{
+	RegNum4x[reg_adress] = value;
+	cmTrBuf0[1] = cmRcBuf0[1];
+	cmTrBuf0[2] = cmRcBuf0[2];
+	cmTrBuf0[3] = cmRcBuf0[3];
+	cmTrBuf0[4] = cmRcBuf0[4];
+	cmTrBuf0[5] = cmRcBuf0[5];
+	cmTrBuf0[6] = cmRcBuf0[6];
+	cmTrBuf0[7] = cmRcBuf0[7];
+	return 8;
+	} //end if()
+else
+	{
+	return ErrorMessage(0x03);//Значение, содержащееся в поле данных запроса, является недопустимой величиной
+	}
+return 0;
+} //end Func06(void)
+
+
+
+
+//-----------------------------Прерывания-------------------------------
+
+//прерывание по завершению получения данных 
+//получение запроса от мастера
+ISR(USART_RXC_vect) 
+{
+char cTempUART;
+
+cTempUART=UDR;
+
+if  (UCSRA&(1<<FE)) //FE-ошибка кадра   ***может не работать на других МК****
+	{
+	UDR = 0xFE; 
+	return;
+	}   
+
+if (!StartRec)//если это первый байт, то начинаем прием
+	{
+	StartRec=true;
+	RcCount=0;
+	cmRcBuf0[RcCount++]=cTempUART;
+	StartTimer0();
+	}
+else  // (StartRec==0) //продолжаем прием
+	{
+    if (RcCount<MAX_LENGHT_REC_BUF) //если еще не конец буфера
+		{
+		cmRcBuf0[RcCount++]=cTempUART;
+		}
+	else //буфер переполнен
+		{
+		cmRcBuf0[MAX_LENGHT_REC_BUF-1]=cTempUART;
+		}
+    StartTimer0();//перезапуск таймера
+	} //end else if (StartRec==0)
+} //end ISR(USART_RXC_vect)
+
+
+//прерывание по опустошению регистра данных (UDR)
+//отправка ответа
+ISR(USART_UDRE_vect)
+{
+if (TrCount<cNumTrByte0)
+	{
+    UDR=cmTrBuf0[TrCount];
+    TrCount++;
+	} //end if
+else
+	{
+    StopTrans();
+    TrCount=0;
+	} //end else
+}//end ISR(USART_UDRE_vect)
+
+
+//прерывание таймера/счетчика 0 по переполнению
+//проверка окончания приема посылки, тишина >=3,5 символа
+ISR(TIMER0_OVF_vect)
+{
+if (StartRec)
+	{
+    StartRec=false; //посылка принята
+    cNumRcByte0=RcCount;  //кол-во принятых байт
+	bModBus=true;//
+    StopTimer0();//остановим таймер
+	} //end if
+}//end ISR(TIMER0_OVF_vect)
+
---
+++ AVR_ModBus.h
@@ -0,0 +1,82 @@
+/*
+* Код для работы по протоколу modbus
+* 
+* void main( void )
+* {
+* InitModBus();
+* sei(); //глобальное разрешение прерываний
+* while(1)
+*   {
+*	CheckModBus();
+*   } //end while(1)
+* } //end main()
+* 
+* необходимо провести инициализацию протокола InitModBus(), и периодически запускать 
+* функцию CheckModBus() для проверки наличия запроса и формирования/отправки ответа.
+* данные заранее вносятся в глобальные переменные RegNum0x[], RegNum1x[], RegNum3x[],
+* RegNum4x[] и беруться из них же. Переменные RegNumYx[] соответствуют номерам регистров:
+* Yx
+* 1 - 9999 - 	Чтение-запись Discrete Output Coils (DO)
+* 10001-19999 - Чтение	Discrete Input Contacts	(DI)
+* 30001-39999 - Чтение	Analog Input Registers	(AI)
+* 40001-49999 - Чтение-запись	Analog Output Holding Registers	(AO)
+* количество регистров задается в одно-байтной переменной QuantityRegYx, Y - 0,1,3,4
+* c адресацией соответственно от 0 до 256.
+*/
+
+#define F_CPU 8000000L
+
+//UART
+#define BAUD_RATE 9600L
+#define BAUD_DIVIDER (F_CPU/(16*BAUD_RATE)-1)
+
+#define UBRRHi  UBRRH
+#define UBRRLow UBRRL
+#define UCSRA_CLR() UCSRA=0
+#define UCSRB_SET() UCSRB=1<<RXEN|1<<TXEN|1<<RXCIE|0<<TXCIE
+#define UCSRC_SET() UCSRC=1<<URSEL|1<<UCSZ0|1<<UCSZ1 //8bit, Even, 1stop.bit
+
+//ModBus
+//ID оборудования
+#define SLAVE_ID 0x0A
+//максимальный размер буфера принимаемых(Rx)/передаваемых(Tx) по UART данных, байт.
+#define MAX_LENGHT_REC_BUF 25
+#define MAX_LENGHT_TR_BUF 25
+//количество регистров
+#define QUANTITY_REG_0X 4 //количество дискретных выходов (DO) / r0x01, w0x05, w0x0F(15)
+#define QUANTITY_REG_1X 2 //количество дискретных входов (DI) / r0x02
+#define QUANTITY_REG_3X 4 //количество аналоговых входов (AI) / r0x04
+#define QUANTITY_REG_4X 6 //количество аналоговых выходов (AO) r0x03, w0x06, w0x10(16)
+
+//запуск таймера0 для контроля паузы 3,5символа
+#define StartTimer0() TCCR0=1<<CS02;  TCNT0=130; 
+// CS02 CS01 CS00 clk/n
+//  0    0    1   clk/1
+//  0    1    0   clk/8
+//  0    1    1   clk/64
+//  1    0    0   clk/256
+//  1    0    1   clk/1024
+//MaxPause t3_5= 3.5*(1+8+1+1)/baud  -> TCNT0 = 256-(clkTn*t3_5)
+//где clkTn = F_CPU / 1 (8, 64, 256, 1024)
+// если baud >= 19200, то t3_5 = 1.75ms
+//TCNT0 = 200  для 8Mhz/256 baud >= 19200
+//TCNT0 = 130  для 8Mhz/256 baud = 9600  
+
+//остановка таймера0
+#define StopTimer0() TCCR0 = 0; TCNT0=0;
+//разрешаем прерывание по переполнению таймера0
+#define Timer0InterruptEnable() TIMSK=1<<TOIE0;
+//разрешить прерывание по освобождению буфера передачи, начать передачу
+#define StartTrans() UCSRB |= (1<<TXEN); UCSRB |= (1<<UDRIE);//SetBit(UCSRB,TXEN); SetBit(UCSRB,UDRIE);
+//запретить прерывание по освобождению буфера передачи, остановка передачи
+#define StopTrans() UCSRB &= ~(1<<TXEN); UCSRB &= ~(1<<UDRIE);//ClrBit(UCSRB,TXEN); ClrBit(UCSRB,UDRIE);
+
+
+//регистры (modbus)  
+unsigned char RegNum0x[(QUANTITY_REG_0X+7)/8];//1-9999 Discrete Output Coils
+unsigned char RegNum1x[(QUANTITY_REG_1X+7)/8];//10001-19999 Discrete Input Contacts
+unsigned int RegNum3x[QUANTITY_REG_3X];//30001-39999 Analog Input Registers
+unsigned int RegNum4x[QUANTITY_REG_4X];//40001-49999 Analog Output Holding Registers
+
+void InitModBus(void);
+void CheckModBus(void);
